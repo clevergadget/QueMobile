062571827109019fb161f5d610aa82d446db1c80
Phillip Dodson <clevergadget@gmail.com>
2012-01-14 22:30:03 -0500

    Testing Login

--- gen/plchldr/quemobile/R.java
+++ gen/plchldr/quemobile/R.java
@@ -11,10 +11,30 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int black_white_gradient=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int qanda=0x7f020002;
+        public static final int question_and_answer=0x7f020003;
+        public static final int quetux=0x7f020004;
+    }
+    public static final class id {
+        public static final int divider=0x7f05000b;
+        public static final int editText1=0x7f050008;
+        public static final int editText2=0x7f05000a;
+        public static final int imageView1=0x7f050000;
+        public static final int linearLayout1=0x7f050001;
+        public static final int linearLayout2=0x7f050003;
+        public static final int password=0x7f050004;
+        public static final int response=0x7f050006;
+        public static final int submit=0x7f050005;
+        public static final int textView1=0x7f050007;
+        public static final int textView2=0x7f050009;
+        public static final int username=0x7f050002;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int login=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int question=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- src/plchldr/quemobile/QueMobileActivity.java
+++ src/plchldr/quemobile/QueMobileActivity.java
@@ -1,13 +1,70 @@
 package plchldr.quemobile;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class QueMobileActivity extends Activity {
+	EditText username;
+	EditText password;
+	Button submit;
+	TextView responsetext;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.login);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        submit = (Button) findViewById(R.id.submit);
+        responsetext = (TextView) findViewById(R.id.response);
+        submit.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				try{
+					DefaultHttpClient client = new DefaultHttpClient();
+					HttpPost loginPost = new HttpPost("http://www.scope-resolution.org/que/login.php");
+					List postValues = new ArrayList();
+					postValues.add(new BasicNameValuePair("username", username.getText().toString()));
+					postValues.add(new BasicNameValuePair("password", password.getText().toString()));
+					UrlEncodedFormEntity loginForm = new UrlEncodedFormEntity(postValues, HTTP.UTF_8);
+					loginPost.setEntity(loginForm);
+					HttpResponse response=client.execute(loginPost);
+					InputStream isResponseData = response.getEntity().getContent();
+					InputStreamReader reader = new InputStreamReader(isResponseData);
+					BufferedReader buffer = new BufferedReader(reader);
+					StringBuilder stringBuilder = new StringBuilder();
+					String currentLine = "";
+					while ( (currentLine=buffer.readLine()) != null) {
+						stringBuilder.append(currentLine + "\n");
+					}
+					responsetext.setText(stringBuilder.toString());
+				}catch (Exception e){
+					e.printStackTrace();
+				}
+				
+			}
+        	
+        });
     }
 }
